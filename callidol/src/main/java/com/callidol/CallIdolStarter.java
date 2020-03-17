5517dba581a4e376103d7c30f74e011784ffa770
fzz <245369629@qq.com>
2020-03-17 17:44:56 +0800

    添加Spring boot框架

---
+++ callidol/src/main/java/com/callidol/CallIdolStarter.java
@@ -0,0 +1,12 @@
+package com.callidol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CallIdolStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(CallIdolStarter.class, args);
+	}
+}
